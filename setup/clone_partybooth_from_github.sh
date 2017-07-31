2326a18df3ed3a38e2ee642beaff67dc1f814300
Bernd <serrin@posteo.de>
2017-07-31 21:12:57 +0200

    Setup Script: setup git defaults and clone

---
+++ setup/clone_partybooth_from_github.sh
@@ -0,0 +1,4 @@
+git config --global user.email "serrin@posteo.de"
+git config --global user.name "Bernd"
+git config --global push.default simple
+git clone https://github.com/bfassbender/partybooth.git ~/partybooth
